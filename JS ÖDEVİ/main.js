aa69f25ebc431ad4ed95aa55fe50e6bb0a4fbdbc
rumeysaarslan1 <rumeysaaa.arslan@gmail.com>
2024-12-14 14:09:17 +0300

    Add files via upload

---
+++ JS ÖDEVİ/main.js
@@ -0,0 +1,18 @@
+let sayi = -5;
+
+if (sayi > 0) {
+  console.log("Sayı pozitiftir.");
+} else if (sayi < 0) {
+  console.log("Sayı negatiftir.");
+} else {
+  console.log("Sayı sıfırdır.");
+}
+let sayi1 = 10;
+let sayi2 = 5;
+
+let toplam = sayi1 + sayi2;
+console.log("Toplam: " + toplam);
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+  
